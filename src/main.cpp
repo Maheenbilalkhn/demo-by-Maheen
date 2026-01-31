5ad9c30d8254d6e8ffeaf8f37672972686bce902
Maheenbilalkhn <maheenbilalkk0@gmail.com>
2026-01-31 22:09:49 +0500

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // My personal information program
+    string name = "Maheen Bilal";
+    string fatherName = "Bilal Khan";
+    string school = "Army Public School and College System (APSACS)";
+    string college = "APSACS (Tufail Shaheed)";
+    string university = "University of Engineering and Technology Mardan";
+    string program = "BS SE (Software Engineering)";
+    string email = "maheenbilalkk0@gmail.com";
+
+    cout << "Name: " << name << endl;
+    cout << "Father's Name: " << fatherName << endl;
+    cout << "School: " << school << endl;
+    cout << "College: " << college << endl;
+    cout << "University: " << university << endl;
+    cout << "Program: " << program << endl;
+    cout << "Email: " << email << endl;
+
+    return 0;
+}
